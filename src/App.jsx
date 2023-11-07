64b1b315357b22812547f41efb659f4387a55ac9
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-11-07 14:19:14 +0200

    add validation and connect to API

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
+
 function App() {
+
+  const [getCountry, setGetCountry] = useState();
+
+  async function searchCountry(country) {
+    try {
+      const response = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+      if (response.ok) {
+        const countryData = await response.json();
+        // setGetCountry(countryData[0]); 
+        console.log(countryData);
+      }
+      else {
+        setGetCountry(null);
+      }
+
+    } catch {
+      setGetCountry(null);
+    }       
+  }
+
+  function handleSubmit(country) {
+    searchCountry(country);
+  }
+
   return (
     <div>
       <Header />
 
-      <Search />
+      <Search onSearch={handleSubmit}/>
 
       
       <i className='bx bxs-chevron-down'></i>
