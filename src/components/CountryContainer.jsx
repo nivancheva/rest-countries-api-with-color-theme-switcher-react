f20fac1bc9e65e4511155c414b17eae2612af892
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-11-09 09:45:34 +0200

    add CountryContainer component

---
+++ src/components/CountryContainer.jsx
@@ -0,0 +1,18 @@
+import './CountryContainer.css';
+
+export default function CountryContainer( {country} ) {
+    return (
+        <div>
+            <div className='card-data'>
+                <img className='flag-img' src={country.flags.svg} />
+                <div className='card'>
+                    <h3 className='card_country'>{country.name.common}</h3>
+                    <p className='card-data'>Population: <span>{country.population}</span></p>
+                    <p className='card-data'>Region: <span>{country.region}</span></p>
+                    <p className='card-data'>Capital: <span>{country.capital}</span></p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
